c313972d178c20b91e2c0253bb4df7ebbc0fd053
SOOJEONGKIMM <poohlicious0@gmail.com>
2020-06-04 09:16:31 -0700

    working on string parse

--- ssu_crond.c
+++ ssu_crond.c
@@ -41,6 +41,8 @@ void read_timecmd(char *str){
 	memset(month,0,TIME_SIZE);
 	memset(weekday,0,TIME_SIZE);
 	memset(syscmd,0,BUFFER_SIZE);
+	char tokens[TOKEN_CNT][MINLEN];
+
 	int i=0;
 	int len=strlen(str);
 	//min
@@ -51,7 +53,8 @@ void read_timecmd(char *str){
 		a++;
 		i++;
 	}
-	calcultime(min);
+	make_tokens(min,tokens,MIN_ITEM);
+	//calcultime(min);
 	printf("min:%s\n",min);
 	//hour
 	i=j;
@@ -63,7 +66,8 @@ void read_timecmd(char *str){
 		a++;
 		i++;
 	}
-	calcultime(hour);
+	make_tokens(hour,tokens,HOUR_ITEM);
+	//calcultime(hour);
 	printf("hour:%s\n",hour);
 	//day
 	i=j;
@@ -75,7 +79,8 @@ void read_timecmd(char *str){
 		a++;
 		i++;
 	}
-	calcultime(day);
+	make_tokens(day,tokens,DAY_ITEM);
+	//calcultime(day);
 	printf("day:%s\n",day);
 	//month
 	i=j;
@@ -87,7 +92,8 @@ void read_timecmd(char *str){
 		a++;
 		i++;
 	}
-	calcultime(month);
+	make_tokens(month,tokens,MONTH_ITEM);
+	//calcultime(month);
 	printf("month:%s\n",month);
 	//weekday
 	i=j;
@@ -99,7 +105,8 @@ void read_timecmd(char *str){
 		a++;
 		i++;
 	}
-	calcultime(weekday);
+	make_tokens(weekday,tokens,WEEKDAY_ITEM);
+	//calcultime(weekday);
 	printf("weekday:%s\n",weekday);
 	//명령어 입력 '\n' 개행만날때까지 _명령어는 예외처리 안해도됨 
 	i=j;
@@ -114,6 +121,147 @@ void read_timecmd(char *str){
 	printf("sys:%s\n",syscmd);
 
 
+}
+int make_tokens(char *str, char tokens[TOKEN_CNT][MINLEN],int itemcnt){
+	char *start, *end;
+	char tmp[BUFFER_SIZE];
+	int cntslash[BUFFER_SIZE];
+	char *op="*/-,";
+	char *comma=",";
+	char *slash="/";
+	char *bar="-";
+	int row=0;
+	int i;
+	int commacnt=0;
+
+	clear_tokens(tokens);
+	start=str;
+	end=strpbrk(start,op);
+
+	//기호 없이 숫자만 존재 or 독립적'*'
+	if(start==end){
+		if(strlen(start)==1&&!strcmp(start,"*"))//독립적'*'
+			printf("%s %s is '*'str:%s",start,end,str);
+		else
+			printf("%s  ==  %s only numbers\n",start,end);//숫자만 존재 
+	}
+	else{
+		end=strpbrk(start,comma);//','기호로 나누기 
+		if(start==end)//','기호가 없는 경우
+			printf("%s %s no ',' exist.str:%s",start,end,str);
+		else{
+			//','기호 개수 세기 
+			for(i=0;start[i];i++)
+				if(start[i]==',')commacnt++;
+			printf("commacnt:%d\n",commacnt);
+			//start:1-5/2,6-10/3    end:,6-10/3     
+			printf("start:%s  !=  %s ','exists.str:%s\n",start,end,str);//숫자만 존재 
+			//','기호 토큰 나눠주기
+			while(commacnt){
+				while(start<end){
+					printf("POOOOOINTER: *st:%d %s\n",*start,start);
+					printf("PLAY start:%s  !=  %s ','exists.tok:%s\n",start,end,tokens[row]);//숫자만 존재 
+					if(*start!=' ')
+						strncat(tokens[row],start,1);
+					start++;
+				}
+				//start:,6-10/3    end:,6-10/3     token:1-5/2
+				printf("sta:%s  !=  %s ','exists.tok:%s\n",start,end,tokens[row]);//숫자만 존재 
+				//'/'주기기호가 존재한다면 
+				if(strstr(tokens[row],"/")!=NULL){
+					//'/'주기 기호로 나누기 
+					end=strpbrk(tokens[row],slash);
+					strcpy(start,tokens[row]);
+					//start:1-5/2 end:/2  tok:1-5/2
+					printf("slash start:%s end:%s tok:%s\n",start,end,tokens[row]);
+					strncpy(tokens[row+1],start,strlen(start)-strlen(end));
+					strcpy(end,end+1);
+					printf("end:%s\n",end);
+					strncpy(tokens[row+2],end,strlen(end));
+					//tokens[row+1]:1-5    tokens[row+2]:2
+					printf("tokens[row+1]:%s\n",tokens[row+1]);
+					printf("tokens[row+2]:%s\n",tokens[row+2]);
+					//'-'주기기호가 존재한다면 
+					if(strstr(tokens[row+1],"-")!=NULL){
+						end=strpbrk(tokens[row+1],bar);
+						strcpy(start,tokens[row+1]);
+						printf("bar start:%s end:%s tok:%s\n",start,end,tokens[row+1]); 
+						strncpy(tokens[row+3],start,strlen(start)-strlen(end));
+						strcpy(end,end+1);
+						strncpy(tokens[row+4],end,strlen(end));
+						printf("tokens[row+3]:%s\n",tokens[row+3]);//1   1부터
+						printf("tokens[row+4]:%s\n",tokens[row+4]);//5   5까지 
+						count_slash_withbar(tokens[row+3],tokens[row+4],tokens[row+2],cntslash);
+					}
+					else
+						count_slash(tokens[row+1],tokens[row+2],cntslash,itemcnt);
+				}
+				commacnt--;
+			}//','기호개수만큼 while루프돌고 끝 
+		}
+
+	}
+}
+// 숫자-숫자/주기 계산
+void count_slash_withbar(char *startcnt, char *endcnt, char *slash, int *savebuf){
+	int startcntINT=atoi(startcnt);
+	int endcntINT=atoi(endcnt);
+	int slashINT=atoi(slash);
+	int i=0;
+	//int itemcnt=item;
+	startcntINT--;
+	while(endcntINT>=startcntINT){
+		startcntINT+=slashINT;
+		printf("st:%d\n",startcntINT);
+		if(endcntINT<=startcntINT)
+			break;
+		savebuf[i]=startcntINT;
+		printf("save[%d]:%d\n",i,savebuf[i]);
+		i+=2;
+	}
+
+
+	printf("%s부터 %s까지 %s만큼 건너뛰어 %ls에 저장\n",startcnt,endcnt,slash,savebuf);
+	printf("%d부터 %d까지 %d만큼 건너뛰어 %ls에 저장\n",startcntINT,endcntINT,slashINT,savebuf);
+
+}
+//  */주기 계산
+void count_slash(char *cnt, char *slash, int *savebuf,int itemcnt){
+	int Itemcnt=itemcnt;
+	int startcnt;
+	int slashINT=atoi(slash);
+
+	if(itemcnt==MIN_ITEM||itemcnt==HOUR_ITEM||itemcnt==WEEKDAY_ITEM)
+		startcnt=0;//0-59 0-23 0-6
+	else if(itemcnt==DAY_ITEM||itemcnt==MONTH_ITEM)
+		startcnt=1;//1-31 1-12 
+
+	if(!strcmp(cnt,"*")){
+		fprintf(stderr,"weird....not '*' for */주기\n");
+		exit(1);
+	}
+
+	startcnt--;
+	int i=0;
+	while(Itemcnt){
+		startcnt+=slashINT;
+		printf("st:%d\n",startcnt);
+		if(Itemcnt<=startcnt)
+			break;
+		savebuf[i]=startcnt;
+		printf("save[%d]:%d\n",i,savebuf[i]);
+		i+=2;
+	}
+	printf("%s를  %s만큼 건너뛰어 %ls에 저장\n",cnt,slash,savebuf);
+
+}
+int is_number(char c){
+	return (c>='0'&&c<='9');
+}
+void clear_tokens(char tokens[TOKEN_CNT][MINLEN]){
+	int i;
+	for(i=0;i<TOKEN_CNT;i++)
+		memset(tokens[i],0,sizeof(tokens[i]));
 }
 void calcultime(char *timestr){
 	printf("time:%s\n",timestr);
--- ssu_crond.h
+++ ssu_crond.h
@@ -16,10 +16,22 @@
 #define SECOND_TO_MICRO 1000000
 #define BUFFER_SIZE 1024
 #define TIME_SIZE 16
+#define TOKEN_CNT 50
+#define MINLEN 64
+
+#define MIN_ITEM 59
+#define HOUR_ITEM 23
+#define DAY_ITEM 31
+#define MONTH_ITEM 12
+#define WEEKDAY_ITEM 6
 
 struct timeval begin_t, end_t;
 
 void read_cronfile();
 void read_timecmd(char *str);
+int make_tokens(char *str, char tokens[TOKEN_CNT][MINLEN],int itemcnt);
+void count_slash_withbar(char *startcnt, char *endcnt, char *slash, int *savebuf);
+void count_slash(char *cnt, char *slash, int *savebuf,int itemcnt);
+void clear_tokens(char tokens[TOKEN_CNT][MINLEN]);
 void calcultime(char *timestr);
 void startdaemon();
